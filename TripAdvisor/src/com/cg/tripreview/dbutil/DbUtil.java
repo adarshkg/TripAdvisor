10e945f8d4ed6eddef3d9111224bf759295d7215
adarshkg <34620170+adarshkg@users.noreply.github.com>
2019-05-17 10:45:37 +0530

    Add files via upload

---
+++ TripAdvisor/src/com/cg/tripreview/dbutil/DbUtil.java
@@ -0,0 +1,11 @@
+package com.cg.tripreview.dbutil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cg.tripreview.dto.Destination;
+import com.cg.tripreview.dto.Review;
+
+public class DbUtil {
+	public static List<Destination> destinationList = new ArrayList<>();
+}
